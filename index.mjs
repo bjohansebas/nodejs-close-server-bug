3b8c54dfc01369e1462af1f8005e73f9e436eb6b
Daniel Ramos <danielramosacosta1@gmail.com>
2023-10-15 01:15:22 +0200

    simplify example

--- index.mjs
+++ index.mjs
@@ -1,38 +1,17 @@
 import * as http from "node:http";
-
-function listenServerPromised(server, port, hostname) {
-  return new Promise((resolve) => {
-    server.listen(port, hostname, (error) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
-
-function closeServerPromised(server) {
-  return new Promise((resolve, reject) => {
-    console.time("server close");
-    server.close((error) => {
-      console.timeEnd("server close");
-      if (error) {
-        reject(error);
-      } else {
-        resolve();
-      }
-    });
-  });
-}
+import { promisify } from "node:util";
 
 const server = http.createServer(async (req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.write("Hello world");
   res.end();
 });
+const listenPromisied = promisify(server.listen.bind(server));
+const closePromisied = promisify(server.close.bind(server));
 
-await listenServerPromised(server, 0, "127.0.0.1");
+await listenPromisied(0, "127.0.0.1");
 const address = server.address();
 await fetch(`http://${address.address}:${address.port}`);
-await closeServerPromised(server);
+console.time("server close");
+await closePromisied();
+console.timeEnd("server close");
